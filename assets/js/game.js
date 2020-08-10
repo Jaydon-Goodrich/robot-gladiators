e42c40435a2732e8a4fd1a9f140617fa33f80580
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-08-09 21:16:09 -0600

    created fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,38 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
-// First function named fight
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight(){
-    window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-//fight();
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    //check enemy health
+    if(enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    //check player's health
+    //check enemy health
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+
+
+};
+
+fight();
